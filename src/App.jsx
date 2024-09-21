3c6e5c2129ea7554da947312f7ed3f3e0fb197d5
Md Eiasin <ariyaneiasin02@gmail.com>
2024-09-22 03:17:18 +0600

    banner part

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Header from "./Components/Header/Header"
 import Navbar from "./Components/Navbar/Navbar"
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import Banner from "./Components/Banner/Banner"
 function App() {
   
   return (
    <>
     <Header/>
     <Navbar/>
+    <Banner/>
    </>
   )
 }
